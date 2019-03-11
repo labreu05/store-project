0adb32aa3320ae14e9ae3b72a02e5cb8d2bd7432
Leonardo Abreu <leonardoeap@gmail.com>
2019-03-10 23:25:23 -0400

    Add yoga server

---
+++ server/src/resolvers/Mutation.js
@@ -0,0 +1,13 @@
+const mutation = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
+
+module.exports = mutation;
